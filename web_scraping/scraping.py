b4bd9cf52665247f0fb454ae4d65b8ade9e4df10
Francis <mbewef86@gmail.com>
2025-11-28 15:35:30 -0800

    Changed the html variable that was passed which was a string converted into an explicit string
    The change was made because passing a literal string in a panda methid which is a read_html makes the panda library guess which isn't a good practice so the best thing is to be explicit about what you're passing into as the argument

--- web_scraping/scraping.py
+++ web_scraping/scraping.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import requests
+from io import StringIO
+
 url="https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes"
 headers={"User-Agent":"Mozilla/5.0(Windows NT 10.0; Win64; x64)"}
 html=requests.get(url,headers=headers).text
-simpsons=pd.read_html(html)
+simpsons=pd.read_html(StringIO(html))
 print(simpsons)
