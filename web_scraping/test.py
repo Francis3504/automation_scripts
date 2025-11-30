db8c7f7e44d870e337abcc8f47e3bb556149edd8
Francis <mbewef86@gmail.com>
2025-11-29 19:40:44 -0800

    Added the renaming method to the data  that was extracted using the panda's library

--- web_scraping/test.py
+++ web_scraping/test.py
@@ -1,10 +1,4 @@
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-url="https://www.amazon.com/s?k=my+smile+amazon&adgrpid=1339207694522088&hvadid=83700758637092&hvbmt=bb&hvdev=c&hvlocphy=143111&hvnetw=o&hvqmt=b&hvtargid=kwd-83701655950204%3Aloc-196&hydadcr=17360_13697265&mcid=cb2dec98ebe43dfea6c9aafa14bb2437&msclkid=4414a424658f1c5fe115cd0d0d02aa2d&tag=mh0b-20&ref=pd_sl_34x11x96w0_b"
-head={"User-Agent": "Mozila/5 (Windows NT)"}
-html=requests.get(url,headers=head).text
-soup=BeautifulSoup(html,"lxml")
-titles=soup.find_all("span",class_="headline")
-for t in titles:
-    print(t)
+football=pd.read_csv("https://www.football-data.co.uk/mmz4281/2526/E0.csv")
+football.rename(columns={"FTHG":None,"FTAG":"well_fuck"},inplace=True)
+print(football)
